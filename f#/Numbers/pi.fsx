f6ae48b2ef850a80c5479c1ac62dbcf0fe7d4510
Doug Swain <dswain@gmail.com>
2016-03-20 22:26:44 -0400

    An initial attempt at calculating pi using the Nilakantha series.

---
+++ f#/Numbers/pi.fsx
@@ -0,0 +1,14 @@
+let pi n =
+    let calculate n =
+        let fn = float(2 * n)
+
+        if n % 2 = 0
+        then -4.0 / (fn * (fn + 1.0) * (fn + 2.0))
+        else 4.0 / (fn * (fn + 1.0) * (fn + 2.0))
+
+    let terms = seq { for i in 1 .. n -> calculate i }
+    let total = Seq.reduce (+) terms
+    3.0 + total
+    
+let input = 60
+printfn "pi ~= %f" <| pi input
